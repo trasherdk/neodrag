6f528edf4596671946c30d6a8a79239422853bb9
Puru Vijay <devpuruvj@gmail.com>
2022-10-27 13:11:16 +0530

    Push

---
+++ packages/svelte/demo/src/routes/mem/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { draggable } from '@neodrag/svelte';
+</script>
+
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
+<div use:draggable>Hello</div>
